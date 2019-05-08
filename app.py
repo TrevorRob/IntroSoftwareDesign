85b2c163a1b87783bf4f37f10ee83a3145e0507e
Amanda Fries <amanda.fries@utexas.edu>
2019-05-08 20:22:11 +0000

    i think i may have added some more stuff here

--- app.py
+++ app.py
@@ -5,22 +5,14 @@ import datetime
 
 app = Flask(__name__)
 
-# @app.route('/', methods=['POST'])
+@app.route('/jobs', methods=['POST'])
+def jobs_api():
+    try:
+        job = request.get_json(force=True)
+    except Exception as e:
+        return True, json.dumps({'status': "Error", 'message':'Invalid JSON: {}.', format(e)})
+    return json.dumps(jobs.add_job(job['start', job['end']))
+
+
     # takes stuff from the url in the curl and puts it into a json (new_job) and puts the jobID in the queue
-
-def generate_jobID():
-    return str(uuid.uuid4())
-
-def generate_job_key(jobID):
-    return 'job.{}'.format(jobID)
-
-# status = [ 'pending', 'in progress', 'complete' ]
-
-def new_job(jobID, CMD, pm):
-    time = str(datetime.datetime.now())
-    return {'id': jobID,
-            'status': 'new',
-            'time': time,
-            'command': CMD,
-            'parameters': pm }
     
