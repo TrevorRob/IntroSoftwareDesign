a16fb299880842ac5d788f2482ac5ea6eaabdb33
trevorRob <trr872@utexas.edu>
2019-05-08 19:12:39 +0000

    added jobs.py

---
+++ jobs.py
@@ -0,0 +1,46 @@
+#job.py
+def generate_jid():
+    return str(uuid.uuid4())
+
+def generate_job_key(jid):
+    return 'job.{}'.format(jid)
+
+def instantiate_job(jid, status, start, end):
+    if type(jid) == str:
+        return {'id': jid,
+                'status': status,
+                'start': start,
+                'end': end
+        }
+    return {'id': jid.decode('utf-8'),
+            'status': status.decode('utf-8'),
+            'start': start.decode('utf-8'),
+            'end': end.decode('utf-8')
+    }
+
+def save_job(job_key, job_dict):
+#    """Save a job object in the Redis database."""
+#    rd.hmset(.......)
+
+def queue_job(jid):
+#    """Add a job to the redis queue."""
+#    ....
+
+def add_job(start, end, status="submitted"):
+    """Add a job to the redis queue."""
+    jid = generate_jid()
+    job_dict = instantiate_job(jid, status, start, end)
+    save_job(......)
+    queue_job(......)
+    return job_dict
+
+def update_job_status(jid, status):
+    """Update the status of job with job id `jid` to status `status`."""
+    jid, status, start, end = rd.hmget(generate_job_key(jid), 'id', 'status', 'start', 'end')
+    job = _instantiate_job(jid, status, start, end)
+    if job:
+        job['status'] = status
+        _save_job(_generate_job_key(jid), job)
+    else:
+        raise Exception()
+
