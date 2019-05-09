a01aff6f33ec961ba3243d8ac26696c1817ae275
trevorRob <trr872@utexas.edu>
2019-05-09 16:08:45 +0000

    idk

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ import json
 import uuid
 import datetime
 
+#Flask app
 app = Flask(__name__)
 
-# @app.route('/', methods=['POST'])
+#data from json file
+data = json.load(open('crops_DS.json','r'))
+
+@app.route('/', methods=['POST'])
     # takes stuff from the url in the curl and puts it into a json (new_job) and puts the jobID in the queue
 
 def generate_jobID():
