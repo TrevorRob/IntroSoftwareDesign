3a7bcebe3779af69adc422243ebfcaff1e172016
Amanda Fries <amanda.fries@utexas.edu>
2019-05-12 21:36:55 +0000

    changed the app.py

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 from flask import Flask, jsonify, request, abort
-import csv
-import json
-import uuid
-import datetime
+import csv, json, uuid, datetime
+import sys
 import pandas as pd
 
 #Flask app
-app = Flask(__name__)
+app = Flask(__app__)
 
 rd = redis.StrictRedis(host='172.17.0.1', port=6379, db=0)
 
 jl = redis.StrictRedis("job_log", host='172.17.0.1', port=6379, db=2)
 
-#data = json.load(open('crops_DS.json','r'))
-csvfile = open('sunspots.csv', 'r')
-jsonfile = open('file.json', 'w')
+def read_csv_data(csv_file, ik, iv):
+    with open(csv_file, encoding='utf-8') as csvf:
+        csv_data = csv.reader(csvf)
+        return [(r[ik], r[iv]) for r in csv_data]
 
-fieldnames = ("Year","Daily Mean of Sunspots")
-reader = csv.DictReader(csvile,fieldnames)
-out = json.dumps( [row for row in reader] )
-jsonfile.write(out)
-data= jsonfile
+def store_data(conn, data):
+    for i in data:
+        rd.setnx(i[0], i[1])
+    return data
 
-print(jsonfile)
+def data_function():
+    if len(sys.argv) < 2:
+    sys.exit(
+        "Usage: %s sunspots.csv [Year, Mean Daily Sunspots]"
+        % __sunspots__
+    )
+    columns = (0,1) if len(sys.argv) < 4 else (int(x) for x in sys.argv([2:4])
+    data = read_csv_data(sys.argv[1], *columns)
+    return (json.dumps(store_data(rd, data)))
+
+data = data_function()
+
 def put_job_in_log(param, cmd):
     jobs.add_job(param, cmd)
 
@@ -35,16 +44,21 @@ def sunspots():
 
 @app.route('/year/<int:year>', methods=['GET'])
 def get_year_spots(year):
-    param = {'year': year}
-    cmd = "get_year()"
-    put_job_in_log(param, cmd)
-    #the year may need to be a string
+#     param = {'year': year}
+#    cmd = "get_year()"
+#    put_job_in_log(param, cmd)
+    return jsonify(data['Year'][year])
 
 @app.route('/max', methods=['GET'])
 def get_max_spots():
-    param = "want year of max sunpots"
-    cmd = "get_max()"
-    put_job_in_log(param, cmd)
+#    param = "want year of max sunpots"
+#    cmd = "get_max()"
+#    put_job_in_log(param, cmd)
+    mean = []
+    for year in data:
+        mean.append(data['Mean Daily Sunspots'])
+    return max(mean)
+
 
 @app.route('/min', methods=['GET'])
 def get_min_spots():
@@ -58,7 +72,7 @@ def jobs_api():
         job = request.get_json(force=True)
     except Exception as e:
         return True, json.dumps({'status': "Error", 'message':'Invalid JSON: {}.', format(e)})
-    return json.dumps(jobs.add_job(job['start', job['end']))
+    return json.dumps(jobs.add_job(job['start'], job['end']))
             
             #im not exactly sure what this is for and we may need to change it to at least fit the rest of the routes
 # takes stuff from the url in the curl and puts it into a json (new_job) and puts the jobID in the queue
