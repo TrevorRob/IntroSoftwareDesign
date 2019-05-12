c2a3b317708d2548b8573b7668f6dcc19640e180
trevorRob <trr872@utexas.edu>
2019-05-12 22:26:09 +0000

    worker

--- worker.py
+++ worker.py
@@ -2,17 +2,22 @@ import redis
 import json
 import hotqueue
 from hotqueue import HotQueue
-import pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #worker
 
 rd = redis.StrictRedis(host='172,17.0.1', port=6379, db=0)
 
-q = HotQueue("queue", host='172.17.0.1', port=6379 db=1)
+q = HotQueue("queue", host='172.17.0.1', port=6379, db=1)
 
 jl = redis.StrictRedis("job_log", host='172.17.0.1', port=6379, db=2)
 
 @q.worker
 def execute_job(jid):
 # add stuff here
+    jobs.update_job_status(jid,running)
+    rd.get()
+
 
